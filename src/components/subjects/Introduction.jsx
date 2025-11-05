f19de3b3778e6a5710685003ae2e7cad9f1cad48
madortill <mador.till@gmail.com>
2025-11-05 18:01:39 +0200

    changes 5/11 7

--- src/components/subjects/Introduction.jsx
+++ src/components/subjects/Introduction.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style/introduction.css";
 import myData from "../../data/myData.json";
 
 function Introduction() {
   const titleIntro = myData.subjMap[3].text;
   const secTitleIntro = myData.introduction[0].text;
+  const sentenceIntro1 = myData.introduction[1].text;
+  const sentenceIntro2 = myData.introduction[2].text;
+  const sentenceIntro3 = myData.introduction[3].text;
+
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const nextPage = () => {
+    setPageIndex((prev) => Math.min(prev + 1, 1));
+    console.log(pageIndex); // יש רק 2 עמודים
+  };
+
+  // מעבר אחורה
+  const prevPage = () => {
+    setPageIndex((prev) => Math.max(prev - 1, 0));
+    console.log(pageIndex);
+  };
 
   return (
     <div className="intro-container">
       <p className="title-introduction">{titleIntro}</p>
-      <div className="intro-page1"></div>
-      <div className="intro-page2">
-        <p className="sec-title-intro">{secTitleIntro}</p>
-        <div className="sign-crash">
-          <ul className="my-list">
-            <li>deyged</li>
-            <li>ghc</li>
-            <li>dhdc</li>
-          </ul>
-        </div>
+      {pageIndex === 0 && (
+  <div className="intro-page1">
+    <p>תוכן של עמוד ראשון — לדוגמה הסבר ראשוני</p>
+  </div>
+)}
+{pageIndex === 1 && (
+  <div className="intro-page2">
+    <p className="sec-title-intro">{secTitleIntro}</p>
+    <div className="sign-crash">
+      <ul className="my-list">
+        <li>{sentenceIntro1}</li>
+        <li>{sentenceIntro2}</li>
+        <li>{sentenceIntro3}</li>
+      </ul>
+    </div>
+  </div>
+)}
+                  <div className="nav-buttons">
+        <button className="nav-button1" onClick={prevPage} disabled={pageIndex === 0}>
+           הקודם
+        </button>
+        <button className="nav-button2" onClick={nextPage} disabled={pageIndex === 1}>
+          הבא 
+        </button>
       </div>
     </div>
+    
   );
 }
 
